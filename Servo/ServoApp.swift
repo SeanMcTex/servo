3c11026f83b40be55fbcb7ad6ddf80b87cb1f920
Sean McMains <sean@mcmains.net>
2026-04-02 13:46:48 -0500

    Initial Commit

---
+++ Servo/ServoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServoApp.swift
+//  Servo
+//
+//  Created by Sean Mc Mains on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ServoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
